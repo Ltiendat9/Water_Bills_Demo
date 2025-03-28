36bccd0e73dab439492ca4ab625a2f84f6f86658
Q <nhatquang2106qh@gmail.com>
2025-03-28 16:13:59 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace Water_Bills
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            addInvoiceUserControl.Hide();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void AddInvoiceMenuButton_Click(object sender, EventArgs e)
+        {
+            addInvoiceUserControl.Show();
+            AddInvoiceMenuButton.BackColor = Color.DarkGray;
+            AddInvoiceMenuButton.ForeColor = Color.White;
+
+                
+        }
+    }
+}
